bfc417f19dde8d6a7a6c71b540e89481bd8b5e10
Aliaksei Palkhouski <a.palkhouski2@gmail.com>
2023-11-05 17:28:49 +0100

    Module 6

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://pejqg70379.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://q2ydhgtv95.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://yjv20yd3x6.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
